a76f5522bd4c5f592ba28f2c9972ba829b33b2ef
Rajveer123 <rajveersingh.charan@gmail.com>
2025-01-30 11:54:16 +0530

    Introduce Routing Mechanism in code and decoupled components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ExamplepagesComponent } from "./examplepages/examplepages.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ExamplepagesComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SamplepagesComponent } from './samplepages/samplepages.component';
+import { DemopagesComponent } from './demopages/demopages.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ExamplepagesComponent } from './examplepages/examplepages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'demopages',
+    component: DemopagesComponent,
+},
+{
+    path: 'examplepages',
+    component : ExamplepagesComponent,
+},
+{
+    path: 'samplepages',
+    component: SamplepagesComponent,
+},
+{
+    path: 'login',
+    component: LoginComponent,
+}, 
+{
+    path: 'profile',
+    component: ProfileComponent,
+},
+{
+    path: 'about',
+    component: AboutComponent,
+}, 
+ {
+    path: 'contactus',
+    component: ContactusComponent,
+}];
