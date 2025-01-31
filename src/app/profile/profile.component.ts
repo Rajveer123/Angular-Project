585412b64f35592ebf25039734a1ef3d50d9f2f6
Rajveer123 <rajveersingh.charan@gmail.com>
2025-01-31 14:25:51 +0530

    Demo for Reactive Form type

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  name = new FormControl('rajveer');
+  email = new FormControl('test@test.com');
   username:string | null = "";
   constructor(private route : ActivatedRoute){}
   ngOnInit(){
     this.username = this.route.snapshot.paramMap.get('name');
+  }
+  updateData(){
+    this.name.setValue('defaultName');
+    this.email.setValue('default@email.com');
+  }
+  submitData(){
+    alert('Form Submitted with values i.e name = ' + this.name.value + ' email = ' + this.email.value);
   }
 }
