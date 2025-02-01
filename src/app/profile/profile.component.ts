8ef2887f1cab9ee6311ff586fb62fb14af831cb8
Rajveer123 <rajveersingh.charan@gmail.com>
2025-02-01 11:17:26 +0530

    Introduce formGroup and formControl to Submit form Data.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-profile',
@@ -7,19 +7,26 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
-  name = new FormControl('rajveer');
-  email = new FormControl('test@test.com');
+export class ProfileComponent implements OnInit {
+  profileForm = new FormGroup({
+    name: new FormControl<string>('', Validators.required),
+    email: new FormControl<string>('',[Validators.required, Validators.email])
+  });
   username:string | null = "";
   constructor(private route : ActivatedRoute){}
   ngOnInit(){
     this.username = this.route.snapshot.paramMap.get('name');
+    
   }
   updateData(){
-    this.name.setValue('defaultName');
-    this.email.setValue('default@email.com');
+    this.profileForm.controls.name.setValue('defaultName');
+    this.profileForm.controls.email.setValue('default@email.com');
   }
-  submitData(){
-    alert('Form Submitted with values i.e name = ' + this.name.value + ' email = ' + this.email.value);
+  handleSubmit(){
+    if (this.profileForm.valid) {
+      alert('Form Submitted with name :'+ this.profileForm.value.name+' and email :'+ this.profileForm.value.email+'.');
+    } else {
+      alert('Form is invalid! Please fill all fields.');
+    }
   }
 }
